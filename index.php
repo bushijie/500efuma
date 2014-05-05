f52e3054875f554a4ee0e97a3085e722a3a02236
Saki <zha_zha@outlook.com>
2014-05-05 11:26:15 +0800

    500efuma

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+//开启调试模式
+define('APP_DEBUG', true);
+define('APP_NAME','App'); //项目名称
+define('APP_PATH','./App/'); //项目路径
+define('RUNTIME_PATH',APP_PATH  . 'Runtime/'); //定义编译目录位置，即Runtime目录
+define('ENGINE_NAME','cluster');//App Engine调用
+include './ThinkPHP/ThinkPHP.php'; //引用ThinkPHP核心运行文件
+?>

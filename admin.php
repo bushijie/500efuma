90fcc2edcd60a386e9d72a75852a601d6e5a4c9f
Saki <zha_zha@outlook.com>
2014-05-13 10:47:44 +0800

    第一个版本整理

--- admin.php
+++
@@ -1,9 +0,0 @@
-<?php
-//开启调试模式
-define('APP_DEBUG', true);
-define('APP_NAME','Admin'); //项目名称
-define('APP_PATH','./Admin/'); //项目路径
-define('RUNTIME_PATH',APP_PATH  . 'Runtime/'); //定义编译目录位置，即Runtime目录
-define('ENGINE_NAME','cluster');//App Engine调用
-include './ThinkPHP/ThinkPHP.php'; //引用ThinkPHP核心运行文件
-?>

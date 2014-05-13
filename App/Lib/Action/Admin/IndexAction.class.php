1d4e2d05a60b899f38f52582b795c71b1e831d9d
Saki <zha_zha@outlook.com>
2014-05-13 11:41:51 +0800

    第二版本，使用了分组配置

---
+++ App/Lib/Action/Admin/IndexAction.class.php
@@ -0,0 +1,31 @@
+<?php
+/** 
+* 文件描述 
+* @author Saki <zha_zha@outlook.com>
+* @date 2014-5-13 上午11:22:08 
+* @version 1.0.0 
+*/ 
+class IndexAction extends Action {
+
+	/** 
+	* 方法注释 
+	* @author Saki <zha_zha@outlook.com>
+	* @date 2014-5-13上午11:22:16 
+	* @version v1.0.0 
+	*/
+	public function index() {
+		$this->display ();
+	}
+	
+	/** 
+	* 登陆方法 
+	* @author Saki <zha_zha@outlook.com>
+	* @date 2014-5-13上午11:22:23 
+	* @version v1.0.0 
+	*/	
+	public function login(){
+		$this->display();
+	}
+	
+	
+}
---
+++ App/Lib/Action/Home/IndexAction.class.php
@@ -0,0 +1,40 @@
+<?php
+/** 
+* 前端页面的入口控制类
+* @author Saki <zha_zha@outlook.com>
+* @date 2014-5-13 上午10:29:39 
+* @version 1.0.0 
+*/ 
+class IndexAction extends Action {
+	
+	/** 
+	* 首页显示 
+	* @author Saki <zha_zha@outlook.com>
+	* @date 2014-5-13上午10:29:39 
+	* @version v1.0.0 
+	*/
+	public function index(){
+		$this->display();
+	}
+	
+	/** 
+	* 文章列表
+	* @author Saki <zha_zha@outlook.com>
+	* @date 2014-5-13上午10:30:51 
+	* @version v1.0.0 
+	*/
+	public function postList(){
+		$this->display();
+	}
+	
+	/** 
+	* 关于我
+	* @author Saki <zha_zha@outlook.com>
+	* @date 2014-5-13上午10:31:10 
+	* @version v1.0.0 
+	*/
+	public function aboutMe(){
+		$this->display();
+	}
+	
+}

706419513f77da98cdc90dc3ebd26c6e0ca9c1d5
李金达 <ilulu4ever816@gmail.com>
2014-12-02 18:05:37 +0800

    change use Thinkphp 3.2

--- index.php
+++ index.php
@@ -1,9 +1,26 @@
 <?php
-//开启调试模式
-define('APP_DEBUG', true);
-define('APP_NAME','App'); //项目名称
-define('APP_PATH','./App/'); //项目路径
-define('RUNTIME_PATH',APP_PATH  . 'Runtime/'); //定义编译目录位置，即Runtime目录
-define('ENGINE_NAME','cluster');//App Engine调用
-include './ThinkPHP/ThinkPHP.php'; //引用ThinkPHP核心运行文件
-?>
+// +----------------------------------------------------------------------
+// | ThinkPHP [ WE CAN DO IT JUST THINK ]
+// +----------------------------------------------------------------------
+// | Copyright (c) 2006-2014 http://thinkphp.cn All rights reserved.
+// +----------------------------------------------------------------------
+// | Licensed ( http://www.apache.org/licenses/LICENSE-2.0 )
+// +----------------------------------------------------------------------
+// | Author: liu21st <liu21st@gmail.com>
+// +----------------------------------------------------------------------
+
+// 应用入口文件
+
+// 检测PHP环境
+if(version_compare(PHP_VERSION,'5.3.0','<'))  die('require PHP > 5.3.0 !');
+
+// 开启调试模式 建议开发阶段开启 部署阶段注释或者设为false
+define('APP_DEBUG',True);
+
+// 定义应用目录
+define('APP_PATH','./Application/');
+
+// 引入ThinkPHP入口文件
+require './ThinkPHP/ThinkPHP.php';
+
+// 亲^_^ 后面不需要任何代码了 就是如此简单

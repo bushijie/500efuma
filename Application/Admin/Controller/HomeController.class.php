61fe3a74b4722d329bb1ea0eb65328ac63a3a90d
李金达 <ilulu4ever816@gmail.com>
2014-12-11 18:57:25 +0800

    添加注释

--- Application/Admin/Controller/HomeController.class.php
+++ Application/Admin/Controller/HomeController.class.php
@@ -2,8 +2,20 @@
 namespace Admin\Controller;
 use Think\Controller;
 
+/**
+ * @ClassName: Admin\Controller$HomeController 
+ * @Description: 后台iframe入口页面【后台首页】
+ * @author Saki <ilulu4ever816@gmail.com>
+ * @date 2014-12-11 下午6:54:46 
+ */
 class HomeController extends Controller{
 	
+	/**
+	 * @todo: 后台首页 
+	 * @author Saki <ilulu4ever816@gmail.com>
+	 * @date 2014-12-11 下午6:56:48 
+	 * @version V1.0
+	 */
 	public function index(){
 		$this->display();
 	}
--- Application/Admin/Controller/IndexController.class.php
+++ Application/Admin/Controller/IndexController.class.php
@@ -1,6 +1,13 @@
 <?php
 namespace Admin\Controller;
 use Think\Controller;
+
+/**
+ * @ClassName: Admin\Controller$IndexController 
+ * @Description: 后台主入口处理
+ * @author Saki <ilulu4ever816@gmail.com>
+ * @date 2014-12-11 下午6:53:58 
+ */
 class IndexController extends AdminBaseController {
 	
 	
@@ -9,6 +16,12 @@ class IndexController extends AdminBaseController {
 		parent::_initialize();
 	}
 	
+	/**
+	 * @todo: 后台入口方法，没有实际的页面，只是根据layout进行渲染
+	 * @author Saki <ilulu4ever816@gmail.com>
+	 * @date 2014-12-11 下午6:54:14 
+	 * @version V1.0
+	 */
     public function index(){
     	$this->display();
     }

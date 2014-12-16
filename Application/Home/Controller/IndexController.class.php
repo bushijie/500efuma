a976173a2de13dcee839f4ac273c26deb37f900a
李金达 <ilulu4ever816@gmail.com>
2014-12-16 19:07:53 +0800

    前端页面初步上传

--- Application/Home/Controller/IndexController.class.php
+++ Application/Home/Controller/IndexController.class.php
@@ -1,8 +1,43 @@
 <?php
 namespace Home\Controller;
 use Think\Controller;
+
+/**
+ * 前端页面的入口控制类
+ * @author Saki <zha_zha@outlook.com>
+ * @date 2014-5-13 上午10:29:39
+ * @version 1.0.0
+ */
 class IndexController extends Controller {
-    public function index(){
-    	$this->display();
-    }
+	
+	
+	/** 
+	* 首页显示 
+	* @author Saki <zha_zha@outlook.com>
+	* @date 2014-5-13上午10:29:39 
+	* @version v1.0.0 
+	*/
+	public function index(){
+		$this->display();
+	}
+	
+	/** 
+	* 文章列表
+	* @author Saki <zha_zha@outlook.com>
+	* @date 2014-5-13上午10:30:51 
+	* @version v1.0.0 
+	*/
+	public function postList(){
+		$this->display();
+	}
+	
+	/** 
+	* 关于我
+	* @author Saki <zha_zha@outlook.com>
+	* @date 2014-5-13上午10:31:10 
+	* @version v1.0.0 
+	*/
+	public function aboutMe(){
+		$this->display();
+	}
 }

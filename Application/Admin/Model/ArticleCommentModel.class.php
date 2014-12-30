473af542686a7b00b4fb5aeaab3a2ba76af40fdd
李金达 <ilulu4ever816@gmail.com>
2014-12-30 20:23:33 +0800

    bug

--- Application/Admin/Model/ArticleCommentModel.class.php
+++ Application/Admin/Model/ArticleCommentModel.class.php
@@ -48,7 +48,6 @@ class ArticleCommentModel extends RelationModel {
 		$model = D('Admin/ArticleComment');
 		if($post['content'] != ''){
 			$data = $post;
-// 			$content = getContent($post['content']);
 			$data['content'] = htmlspecialchars($post['content']);
 			$data['name'] = htmlspecialchars($post['name']);
 			$data['ctm'] = date('Y-m-d H:i:s',time());

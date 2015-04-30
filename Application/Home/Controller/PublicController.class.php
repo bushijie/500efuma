ff17552b568027e9da0a40c55927dc2e3b82b309
Saki <ilulu4ever816@gmail.com>
2015-04-30 12:21:09 +0800

    QQ接入

--- Application/Home/Controller/PublicController.class.php
+++ Application/Home/Controller/PublicController.class.php
@@ -4,12 +4,20 @@ use Think\Controller;
 
 /**
  * @ClassName: Home\Controller$PublicController 
- * @Description: 模板布局文件处理
+ * @Description: 模板布局文件处理以及公共方法调用的处理
  * @author Saki <ilulu4ever816@gmail.com>
  * @date 2014-12-3 下午4:52:06 
  */
 class PublicController extends HomeBaseController{
 
+	/**
+	 * 
+	 * 
+	 */
+	public function qqcallback(){
+		echo '腾讯回调地址';
+	}
+	
 	
 	
 	
